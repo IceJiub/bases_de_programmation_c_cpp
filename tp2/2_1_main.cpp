24f3abca95c3dccce4761e43e8c23c49763612cf
IceJiub <tom.yang@hotmail.fr>
2024-10-27 02:53:24 +0100

    Add 2_1_main.cpp

---
+++ tp2/2_1_main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "main.hpp"
+using namespace std;
+
+int main()
+{
+    int nbGrades;
+    int moyenne;
+
+    cout << "Entrez le nombre de notes : " <<endl;
+    cin >> nbGrades;
+
+    moyenne = mean(nbGrades);
+
+    cout << "La moyenne est : " << moyenne << endl;
+
+    return 0;
+}
