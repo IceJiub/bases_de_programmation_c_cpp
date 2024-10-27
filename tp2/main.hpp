b242bd02961a02f879534cc9dc4d64989807bf3c
IceJiub <tom.yang@hotmail.fr>
2024-10-27 11:29:24 +0100

    Add getPriceWithTaxes, addMargin, addVAT and addEcoTax functions

--- tp2/main.hpp
+++ tp2/main.hpp
@@ -5,5 +5,9 @@ int addGrade();
 int mean(int nbGrades);
 int max(int a, int b);
 int max(int a, int b, int c);
+double getPriceWithTaxes(double prix);
+double addMargin(double prixHT);
+double addVAT(double prixHT);
+double addEcoTax(double prix);
 
 #endif
