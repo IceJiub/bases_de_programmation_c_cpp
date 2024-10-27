badb20eca2ce56814edcde6d24062836b8f93d5d
IceJiub <tom.yang@hotmail.fr>
2024-10-27 11:35:05 +0100

    Modify 2_1_addGrade.cpp to 2_1_grade.cpp, move mean function to 2_1_grade.cpp and delete 2_1_mean.cpp

--- tp2/2_1_grade.cpp
+++ tp2/2_1_grade.cpp
@@ -11,3 +11,11 @@ int addGrade()
     }
     return note;
 }
+
+int mean(int nbGrades) 
+{
+    int somme = 0;
+    for (int i = 1 ; i <= nbGrades ; i++)
+        somme += addGrade();
+    return somme / nbGrades;
+}
