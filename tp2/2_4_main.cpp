596c621aac288182a9ea56d0b51bc3aa7bd11903
IceJiub <tom.yang@hotmail.fr>
2024-10-29 00:21:28 +0100

    Add 2_4_main.cpp

---
+++ tp2/2_4_main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "main.hpp"
+using namespace std;
+
+int main() 
+{
+    int nb_prime;
+
+    cout << "Entrez un nombre : " <<endl;
+    cin >> nb_prime;
+
+    findTwoPreviousPrime(nb_prime);
+    
+    return 0;
+}
