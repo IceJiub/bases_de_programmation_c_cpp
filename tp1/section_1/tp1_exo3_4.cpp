d9153b8b58f6459e18dc390f4bc2f6882a2505ab
IceJiub <tom.yang@hotmail.fr>
2024-10-29 21:34:09 +0100

    Initialize operation and move lines where we choose the operation type inside the loop. Remove line breaks and unnecessary braces.

--- tp1/section_1/tp1_exo3_4.cpp
+++ tp1/section_1/tp1_exo3_4.cpp
@@ -1,39 +1,34 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-
+int main()
+{
     double x;
     double y;
-    int operation;
+    int operation = -1;
 
     cout << "Entrez le premier nombre : " << endl;
     cin >> x;
     cout << "Entrez le deuxième nombre : " << endl;
     cin >> y;
 
-    cout << "Choisissez une opération (0 : quitter, 1 : +, 2 :  -, 3 : *, 4 : /) : " << endl;
-    cin >> operation;
-
-    while (operation != 0) {
+    while (operation != 0) 
+    {
+        cout << "Choisissez une opération (0 : quitter, 1 : +, 2 :  -, 3 : *, 4 : /) : " << endl;
+        cin >> operation;
         if (operation == 1)
             cout << "Résultat : " << x + y << endl;
         else if (operation == 2)
             cout << "Résultat : " << x - y << endl;
         else if (operation == 3)
             cout << "Résultat : " << x * y << endl;
-        else if (operation == 4) {
+        else if (operation == 4)
             if (y == 0)
                 cout << "Division par 0 impossible." << endl;
             else
                 cout << "Résultat : " << x / y << endl;
-        }
-        else
+        else if (operation > 5 || operation < 0)
             cout << "Opération incorrecte." << endl;
-
-        cout << "Choisissez une opération (0 : quitter, 1 : +, 2 :  -, 3 : *, 4 : /) : " << endl;
-        cin >> operation;
     }
 
     cout << "Au revoir." << endl;
