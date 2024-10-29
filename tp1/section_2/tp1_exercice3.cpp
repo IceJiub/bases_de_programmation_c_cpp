3d194cf0c711cbe3c0cbbdfc0cb8fed0aebe4073
IceJiub <tom.yang@hotmail.fr>
2024-10-29 21:46:25 +0100

    Remove line breaks and unecessary braces and place open braces to a new line. Change the last else condition into else if to check if operation is incorrect (inferior to 0 or superior to 5).

--- tp1/section_2/tp1_exercice3.cpp
+++ tp1/section_2/tp1_exercice3.cpp
@@ -1,48 +1,40 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-
+int main() 
+{
     double x;
     double y;
-    int operation = 10;
+    int operation = -1;
 
-    while (operation != 0) {
-
+    while (operation != 0)
+    {
         cout << "Entrez le premier nombre : " << endl;
         cin >> x;
         cout << "Entrez le deuxième nombre : " << endl;
         cin >> y;
-        cout << "Choisissez une opération (0 : quitter, 1 : +, 2 :  -, 3 : *, 4 : /) : " << endl;
-        cin >> operation;
-
-        while (operation != 0) {
+        while (operation != 0)
+        {
+            cout << "Choisissez une opération (0 : quitter, 1 : +, 2 :  -, 3 : *, 4 : /) : " << endl;
+            cin >> operation;
             if (operation == 1) 
                 cout << "Résultat : " << x + y << endl;
             else if (operation == 2) 
                 cout << "Résultat : " << x - y << endl;
             else if (operation == 3)
                 cout << "Résultat : " << x * y << endl;
-            else if (operation == 4) {
+            else if (operation == 4)
                 if (y == 0)
                     cout << "Division par 0 impossible." << endl;
                 else
                     cout << "Résultat : " << x / y << endl;
-            }
-            else
+            else if (operation > 5 || operation < 0)
                 cout << "Opération incorrecte." << endl;
-            cout << "Choisissez une opération (0 : quitter, 1 : +, 2 :  -, 3 : *, 4 : /) : " << endl;
-            cin >> operation;
         }
-
-        if (operation == 0) {
+        if (operation == 0)
             cout << "Voulez-vous quitter (0) ou changer les nombres (1) : " << endl;
             cin >> operation;
-        }
-
     }
-
     cout << "Au revoir." << endl;
 
     return 0;
