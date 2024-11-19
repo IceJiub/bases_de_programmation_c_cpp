541e25afe57f651d3b757617fb65e81c1e9292ba
IceJiub <tom.yang@hotmail.fr>
2024-11-19 17:57:35 +0100

    Add example program to demonstrate usage of displayArray with integer arrays

---
+++ tp4/1_tableaux/ex1_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "array.hpp"
+using namespace std;
+
+int main()
+{
+    int size = 100;
+    int tab[size];
+    displayArray(tab, size);
+    for (int i = 0; i < size; i++)
+        tab[i] = i;
+    displayArray(tab, size);
+}
