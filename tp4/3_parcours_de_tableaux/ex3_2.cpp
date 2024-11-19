c389b967cdc663c699d01613356dfa2eab8d20c6
IceJiub <tom.yang@hotmail.fr>
2024-11-19 23:37:30 +0100

    Add ex3_2 program to demonstrate array input and mean calculation

---
+++ tp4/3_parcours_de_tableaux/ex3_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "../array.hpp"
+using namespace std;
+
+int main()
+{
+    int size = 6;
+    double tab[size];
+    for (int i = 0 ; i < size ; i++)
+        cout << "Entrez la note " << i << " : ", cin >> tab[i];
+    cout << "Tableau : ";
+    displayArray(tab, size);
+    cout << "Moyenne : " << mean(tab, size) << endl;
+}
