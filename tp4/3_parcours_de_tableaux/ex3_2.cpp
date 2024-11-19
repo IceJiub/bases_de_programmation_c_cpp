f9a08a1d8da521a6786427808cdabd0dc67552d7
IceJiub <tom.yang@hotmail.fr>
2024-11-19 23:39:09 +0100

    Fix input prompt to display correct note number in ex3_2 program

--- tp4/3_parcours_de_tableaux/ex3_2.cpp
+++ tp4/3_parcours_de_tableaux/ex3_2.cpp
@@ -7,7 +7,7 @@ int main()
     int size = 6;
     double tab[size];
     for (int i = 0 ; i < size ; i++)
-        cout << "Entrez la note " << i << " : ", cin >> tab[i];
+        cout << "Entrez la note " << i+1 << " : ", cin >> tab[i];
     cout << "Tableau : ";
     displayArray(tab, size);
     cout << "Moyenne : " << mean(tab, size) << endl;
