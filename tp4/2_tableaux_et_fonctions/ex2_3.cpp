b410c14dd664a826947cc5c643a29aa4bf82697a
IceJiub <tom.yang@hotmail.fr>
2024-11-19 22:55:29 +0100

    Refactor ex2_3 to use fillArrayWithPrimeNumbers for prime number population

--- tp4/2_tableaux_et_fonctions/ex2_3.cpp
+++ tp4/2_tableaux_et_fonctions/ex2_3.cpp
@@ -6,10 +6,6 @@ int main()
 {
     int size = 100;
     int tab[size];
-    tab[0] = 1;
-    for (int i = 1; i < size; i++)
-    {
-        tab[i] = getNextPrime(tab[i-1]);
-    }
+    fillArrayWithPrimeNumbers(tab, size);
     displayArray(tab, size);
 }
