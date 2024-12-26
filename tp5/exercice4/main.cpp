6a72924e273f0dcbad04b5bc791f844de5eead0a
IceJiub <tom.yang@hotmail.fr>
2024-12-26 12:12:39 +0100

    Refactor measure additions in main program for PollutingProjectsAnalyzer

--- tp5/exercice4/main.cpp
+++ tp5/exercice4/main.cpp
@@ -7,11 +7,11 @@ int main()
 {
     PollutingProjectsAnalyzer analyzer;
     int id1 = analyzer.addNewProject();
-    analyzer.addMeasure(id1, 130);
+    analyzer.addMeasure(id1, 100);
+    analyzer.addMeasure(id1, 110);
     analyzer.addMeasure(id1, 120);
-    analyzer.addMeasure(id1, 256);
     analyzer.addMeasure(id1, 130);
-    analyzer.addMeasure(id1, 120);
-    analyzer.addMeasure(id1, 256);
+    analyzer.addMeasure(id1, 140);
+    analyzer.addMeasure(id1, 150);
     analyzer.getProject(id1).displayMeasures();
 }
